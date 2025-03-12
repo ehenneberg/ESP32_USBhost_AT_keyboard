4ae19020a900e594227feb84e83c8d7a21bff1a0
ehenneberg <73541863+ehenneberg@users.noreply.github.com>
2025-03-12 14:35:52 +0100

    Add files via upload
    improved PS/2 init timing, cleaned source

--- Usbhost_Keyboard_to_PS2.ino
+++ Usbhost_Keyboard_to_PS2.ino
@@ -35,6 +35,7 @@
 // on command readIdCmd, 2 keybord ID bytes are returned
 #define setStatusCmd    0xED  // set keyboard LED status
 #define diagnosticCmd   0xEE  // diagnostic
+#define set_getScancode 0xF0  // set/get scan code set 2 , only set 2 available !
 #define readIdCmd       0xF2  // read 2 byte keyboard ID
 #define repeatRateCmd   0xF3  // set keyboard repeat key speed
 #define keybEnableCmd   0xF4  // keyboard enable
@@ -45,19 +46,29 @@
 
 // return values as response on motherboard commands
 #define ack         0xFA  // acknowledge returned on PS2 commands, except resend
-#define readIdByte1 0xAB  // readId return value #1
-#define readIdByte2 0x83  // readId return value #2
+#define readIdByte1 0xAB  // readId response value #1
+#define readIdByte2 0x83  // readId response value #2
 #define selfTestOk  0xAA  // returned value after resetKeyboard
+#define scanCodeVal 0x41  // Scan set 2 value response
+
+// PS/2 bus and init timing values
+#define PS2powerUpDelay 5000        // power up delay in msec.
+#define initTimeOut 30000           // PS/2 init timeout in msec.
+#define PS2clkHalfBitTime 20        // PS/2 half clock bit time in usec.
+#define PS2clkBitTime 40            // PS/2 clock bit time => 2 bit clk period ~ 12.5 kHz
+#define delayBeforeRxPS2cmd 250     // delay before clocking in a PS/2 command 
+#define ackResponseDelay 650        // ack send_response delay on a motherboard command
+#define keyboardResetDelay 250000   // emulate keyboard reset delay in usec.
+#define PS2cmdResponseDelay 300     // response delay on PS/2 command in usec.
+#define PS2scanCodeDelay 1500       // delay before sending PS/2 keyboard scan code in usec.
 
 uint8_t PS2cmd, lastPS2cmd, repeatRate, keyboardStatus, setScanCodeVal;
-uint32_t PS2powerUpDelay = 5000;  // power up delay in msec.
-uint32_t initTimeOut = 30000;     // PS/2 init timeout in msec.
-uint32_t previousTime;            // Previous time for timed loops
+
 volatile bool PS2txBits[11];      // PS/2 scan code frame bits transmitted to PC
 
 // PS/2 scan code transmit and send to the UART0 as well via Arduino Serial 
 void sendPS2scanbyte(uint8_t outbyte) {
-  sendPS2byte(outbyte);
+  sendPS2byte(outbyte, PS2scanCodeDelay);
   if (outbyte < 16) { Serial.print("0"); }
   Serial.print(outbyte, HEX);
   Serial.print(" ");
@@ -67,12 +78,12 @@ void sendPS2scanbyte(uint8_t outbyte) {
 // uses the USBtoPS2_conversion_table.h to find PS/2 scan code set 2 data
 // and transmits PS/2 data. 
 void sendPS2scanCode(uint8_t USBkeycode, uint8_t makeBreak, uint8_t modifier) {
-  if (makeBreak == 0) {
-    digitalWrite(greenLedPin, HIGH);   // break => green led off
-  } else {
-    digitalWrite(greenLedPin, LOW);  // make => green led on
-  }
   if (modifier == 0) {  // non modifier key
+    if (makeBreak == 0) {
+      digitalWrite(greenLedPin, HIGH);   // break => green led off
+    } else {
+      digitalWrite(greenLedPin, LOW);  // make => green led on
+    }
     switch (USBkeycode) {
       case 0x48:              // PrtPause ?
         if (makeBreak == 1) {  // PrtPause and Make ? 
@@ -192,32 +203,30 @@ void setup() {
   digitalWrite(PS2clkOutPin, PS2busOutHIGH);  // PS2 clk high
   pinMode(PS2dataOutPin, OUTPUT);   // set PS2 data output pin as output
   digitalWrite(PS2dataOutPin, PS2busOutHIGH); // PS2 data high
-  pinMode(PS2clkInPin, INPUT);      // set PS2 clk input pin as input
-  pinMode(PS2dataInPin, INPUT);     // set PS2 data input pin as input
+  pinMode(PS2clkInPin, INPUT_PULLUP);      // set PS2 clk input pin as input
+  pinMode(PS2dataInPin, INPUT_PULLUP);     // set PS2 data input pin as input
   pinMode(greenLedPin, OUTPUT);    // set PS2 clk output pin as output
   digitalWrite(greenLedPin, LOW);  // green led off
   lastPS2cmd = 0x33;      // no PS2 cmd read yet, so set a non existing cmd
   Serial.println("Enter " + String(initTimeOut/1000) + " sec. PS/2 boot sequence");
   initPS2();      // perform the start-up PS/2 initialization with motherboard 
-  delay(500);
   usbHost.begin();
   usbHost.setHIDLocal(HID_LOCAL_Danish);
-  previousTime = millis();
 }
 
 // send a byte to PS/2 bus ( scan code or response during initialization )
-void sendPS2byte(uint8_t outbyte) {
+void sendPS2byte(uint8_t outbyte, uint32_t beforeDelay) {
   uint8_t outByte = outbyte;
   uint8_t trueBitCnt = 0;
-  while (digitalRead(PS2clkInPin) == LOW) { };  // wait as long as clk is low
-  delayMicroseconds(600);                      // clk high delay for motherboard
+  delayMicroseconds(beforeDelay);               // clk high delay for motherboard
+  while (readPS2bus() != Clk_Data );            // wait clk=1 and data = 1
   digitalWrite(PS2dataOutPin, PS2busOutLOW);    // start bit, data low
-  delayMicroseconds(20);                        // 1/4 period start bit setup
+  delayMicroseconds(PS2clkHalfBitTime);         // 1/4 period start bit setup
   digitalWrite(PS2clkOutPin, PS2busOutLOW);     // neg. clk edge for start bit
-  delayMicroseconds(40);                        // clk low period
+  delayMicroseconds(PS2clkBitTime);             // clk low period
   digitalWrite(PS2clkOutPin, PS2busOutHIGH);    // clk high
   for (int i=0; i<8; i++) {
-    delayMicroseconds(20);                      // 1/4 period data bit setup
+    delayMicroseconds(PS2clkHalfBitTime);       // 1/4 period data bit setup
     if ((outByte >> i) & 0x01) {
       trueBitCnt++;
       digitalWrite(PS2dataOutPin, PS2busOutHIGH);
@@ -225,24 +234,24 @@ void sendPS2byte(uint8_t outbyte) {
     else {
       digitalWrite(PS2dataOutPin, PS2busOutLOW);
     }
-    delayMicroseconds(20);                      // 1/4 period
+    delayMicroseconds(PS2clkHalfBitTime);       // 1/4 period
     digitalWrite(PS2clkOutPin, PS2busOutLOW);   // neg. clk edge for data bit  
-    delayMicroseconds(40);                      // clk low period
+    delayMicroseconds(PS2clkBitTime);           // clk low period
     digitalWrite(PS2clkOutPin, PS2busOutHIGH);  // clk high
   } 
   // send odd parity
-  delayMicroseconds(20);                        // 1/4 period data bit setup
+  delayMicroseconds(PS2clkHalfBitTime);         // 1/4 period data bit setup
   if ((trueBitCnt & 0x01 ) == 0)  { digitalWrite(PS2dataOutPin, PS2busOutHIGH); }
   else { digitalWrite(PS2dataOutPin, PS2busOutLOW); } // set odd parity
-  delayMicroseconds(20);                        // 1/4 period
+  delayMicroseconds(PS2clkHalfBitTime);         // 1/4 period
   digitalWrite(PS2clkOutPin, PS2busOutLOW);     // neg. clk edge for parity bit  
-  delayMicroseconds(40);                        // clk low period
+  delayMicroseconds(PS2clkBitTime);             // clk low period
   digitalWrite(PS2clkOutPin, PS2busOutHIGH);    // clk high  
-  delayMicroseconds(20);                        // 1/4 period stop bit setup
+  delayMicroseconds(PS2clkHalfBitTime);         // 1/4 period stop bit setup
   digitalWrite(PS2dataOutPin, PS2busOutHIGH);   // set stop bit
-  delayMicroseconds(20);                        // 1/4 period
+  delayMicroseconds(PS2clkHalfBitTime);         // 1/4 period
   digitalWrite(PS2clkOutPin, PS2busOutLOW);     // neg. clk edge for stop bit
-  delayMicroseconds(40);                        // clk low period
+  delayMicroseconds(PS2clkBitTime);             // clk low period
   digitalWrite(PS2clkOutPin, PS2busOutHIGH);    // clk high
 }
 
@@ -251,28 +260,28 @@ uint8_t readPS2cmd()
 {
   uint8_t rxByte;
   bool PS2rxBits[11];
-  delayMicroseconds(600);                  // delay for motherboard ahead of getting cmd
+  delayMicroseconds(delayBeforeRxPS2cmd);   // delay for motherboard ahead of getting cmd
   digitalWrite(PS2clkOutPin, PS2busOutLOW); // PS2 clk low
   for (int i=0; i<9; i++) {
-    delayMicroseconds(40);
+    delayMicroseconds(PS2clkBitTime);
     digitalWrite(PS2clkOutPin, PS2busOutHIGH); // PS2 clk high
     PS2rxBits[i] = digitalRead(PS2dataInPin);
-    delayMicroseconds(40);
+    delayMicroseconds(PS2clkBitTime);
     digitalWrite(PS2clkOutPin, PS2busOutLOW); // PS2 clk low
   }
-  delayMicroseconds(40);
+  delayMicroseconds(PS2clkBitTime);
   digitalWrite(PS2clkOutPin, PS2busOutHIGH); // PS2 clk high  
-  delayMicroseconds(20);
+  delayMicroseconds(PS2clkHalfBitTime);
   digitalWrite(PS2dataOutPin, PS2busOutLOW); // PS2 data pin low , ACK start 
-  delayMicroseconds(20);
+  delayMicroseconds(PS2clkHalfBitTime);
   digitalWrite(PS2clkOutPin, PS2busOutLOW); // PS2 clk low for sending ACK
-  delayMicroseconds(40);
+  delayMicroseconds(PS2clkBitTime);
   digitalWrite(PS2clkOutPin, PS2busOutHIGH); // PS2 clk high
-  delayMicroseconds(20);
+  delayMicroseconds(PS2clkHalfBitTime);
   digitalWrite(PS2dataOutPin, PS2busOutHIGH); // PS2 data pin high , ACK end 
-  delayMicroseconds(20);
+  delayMicroseconds(PS2clkHalfBitTime);
   digitalWrite(PS2clkOutPin, PS2busOutLOW); // PS2 clk low for sending ACK
-  delayMicroseconds(40);
+  delayMicroseconds(PS2clkBitTime);
   digitalWrite(PS2clkOutPin, PS2busOutHIGH); // PS2 clk high
   rxByte = 0;
   for (int i=0; i<8; i++) {
@@ -293,43 +302,43 @@ uint8_t readPS2bus() {
 
 // the tricky initialization part with motherboard using PS/2 protocol timing :)
 void initPS2() {
-  previousTime = millis();
+  uint32_t previousTime = millis();
   while ((millis() - previousTime) < initTimeOut) {
     while ((readPS2bus() != nClk_nData) && ((millis() - previousTime) < initTimeOut));  // wait for clk = 0 and data = 0
     while (( readPS2bus() == nClk_nData) && ((millis() - previousTime) < initTimeOut));  // wait while clk = 0 and data = 0
     if ( readPS2bus() == Clk_nData) {  // clk = 1 and data = 0 => PS2cmd ready
       PS2cmd = readPS2cmd();
-      sendPS2byte(ack);   // response on commands, except resendCmd
-      delayMicroseconds(350);           // to cope with a motherboard clk high 60 usec spurious
-      if (readPS2bus() != nClk_nData) {
-      delayMicroseconds(650);           // to cope with a motherboard clk high 60 usec spurious
-      if ( readPS2bus() == Clk_Data )  {   // clk = 1 and data = 1 => PS2bus tx ready for PS2 command response
-        if (PS2cmd == resetKeybCmd) {
-          sendPS2byte(selfTestOk);
+      sendPS2byte(ack, ackResponseDelay);   // response on commands, except resendCmd
+      delayMicroseconds(350);               // to cope with a motherboard processing time ( many clk changes )
+      if (readPS2bus() != nClk_nData) {     // if no new PS2 cmd then
+        delayMicroseconds(650);           
+        if ( readPS2bus() == Clk_Data )  {   // clk = 1 and data = 1 => PS2bus tx ready for PS2 command response
+          if (PS2cmd == resetKeybCmd) {
+            sendPS2byte(selfTestOk, keyboardResetDelay);
+          }
+          if (PS2cmd == readIdCmd) {
+            sendPS2byte(readIdByte1, PS2cmdResponseDelay);
+            sendPS2byte(readIdByte2, PS2cmdResponseDelay);
+          }
+          if (PS2cmd == diagnosticCmd) {
+            sendPS2byte(diagnosticCmd, PS2cmdResponseDelay);
+          }
         }
-        if (PS2cmd == readIdCmd) {
-          sendPS2byte(readIdByte1);
-          sendPS2byte(readIdByte2);
+        if (lastPS2cmd == repeatRateCmd) { // repeat rate value now ?
+          repeatRate = PS2cmd;  
         }
-        if (PS2cmd == diagnosticCmd) {
-          sendPS2byte(diagnosticCmd);
+        if (lastPS2cmd == setStatusCmd) { // set keyboard status now ?
+          keyboardStatus = PS2cmd;  
+          if (keyboardStatus == 0x02) {
+            digitalWrite(greenLedPin, HIGH);  // green led on on NumLock LED on
+          }
         }
-      }
-      if (lastPS2cmd == repeatRateCmd) { // repeat rate value now ?
-        repeatRate = PS2cmd;  
-      }
-      if (lastPS2cmd == setStatusCmd) { // set keyboard status now ?
-        keyboardStatus = PS2cmd;  
-        if (keyboardStatus == 0x02) {
-          digitalWrite(greenLedPin, HIGH);  // green led on on NumLock LED on
+        if (lastPS2cmd == set_getScancode) { // set/get keyboard scan code set ?
+          setScanCodeVal = PS2cmd;
+          if (PS2cmd == 0x00) {   // response with actual scan code set ?
+            sendPS2byte(scanCodeVal, PS2cmdResponseDelay);    // return scan code set 2 
+          }
         }
-      }
-      if (lastPS2cmd == setStatusCmd) { // read keyboard scan code set ?
-        setScanCodeVal = PS2cmd;
-        if (PS2cmd == 0x00) {
-          sendPS2byte(0x41);          // return scan code set 2 
-        }
-      }
       }
       lastPS2cmd = PS2cmd;
     }
